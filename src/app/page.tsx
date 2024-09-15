72ded46b05bf4f2904a18df7cb94767c4876bfab
arief satrio <ariefsatrio098@gmail.com>
2024-09-15 18:00:53 +0700

    fix: bug on hero layouts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,20 +11,20 @@ import React, { useEffect, useState } from "react";
 export default function About(): React.ReactElement {
     return (
         <main>
-            <div className="hero grid text-[9vw] md:text-[5.97vw] grid-cols-12">
-                <div className="col-start-5 md:col-start-6 -col-end-1">
+            <div className="hero grid text-[8.765vw] md:text-[5.57vw] grid-cols-12">
+                <div className="flex justify-end col-start-5 col-end-13 md:col-start-6 md:col-end-11">
                     <span>Arief Satrio is a</span>
                 </div>
-                <div className="col-start-1 md:col-start-3 -col-end-1">
+                <div className="flex justify-start col-start-1 col-end-12 md:col-start-3">
                     <span>Fullstack Developer</span>
                 </div>
-                <div className="col-start-1 md:col-start-3 -col-end-1">
+                <div className="flex justify-center col-start-1 md:col-start-3 -col-end-1 md:col-end-11">
                     <span>and Graphic Designer</span>
                 </div>
-                <div className="col-start-5 md:col-start-6 -col-end-1">
+                <div className="flex justify-end col-start-5 col-end-13 md:col-start-6 md:col-end-11">
                     <span>from Indonesia</span>
                 </div>
-                <div className="col-start-1 md:col-start-3 -col-end-1">
+                <div className="flex justify-start col-start-1 col-end-12 md:col-start-3">
                     <span>based in Bengkulu</span>
                 </div>
             </div>
