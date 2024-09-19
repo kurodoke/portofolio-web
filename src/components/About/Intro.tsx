4d638fa82165943757f3280616854f27eff3d91b
arief satrio <ariefsatrio098@gmail.com>
2024-09-19 14:56:38 +0700

    feat: improve intro style

--- src/components/About/Intro.tsx
+++ src/components/About/Intro.tsx
@@ -1,39 +1,62 @@
+import { fontDisplay, fontDisplayItalic } from "@/font";
 import Link from "next/link";
 import React from "react";
 
 export default function Intro(): React.ReactElement {
     return (
         <section className="intro p-5 md:p-10 bg-custom-yellow border-y-2 border-black">
-            <div className="grid grid-cols-12 text-base md:text-xl">
-                <span className="text-3xl md:text-4xl md:col-start-2 lg:col-start-3 mb-2">
-                    intro
-                </span>
-                <div className="col-start-1 md:col-start-2 lg:col-start-4 col-end-13 md:col-end-11 flex flex-col gap-2">
-                    <p>
-                        Arief is both a full-stack developer and a graphic
-                        designer, blending creativity with technical expertise.
-                        His passion for design shines through bold typography
-                        and a fascination with brutalism, always pushing the
-                        limits of what’s possible visually.
-                    </p>
-                    <p>
-                        On the technical side, Arief develops full-stack
-                        applications that integrate strong front-end design with
-                        reliable back-end infrastructure. With experience in
-                        Google Cloud, he ensures his projects are scalable and
-                        efficient.
-                    </p>
-                    <p>
-                        Whether it's design or development, Arief excels in
-                        crafting solutions that are both beautiful and
-                        functional. Feel free to connect with him at{" "}
-                        <Link
-                            className="underline underline-offset-1"
-                            href={"mailto:ariefsatrio098@gmail.com"}
+            <div className="grid grid-cols-6 gap-y-5 md:grid-cols-12 text-base md:text-xl">
+                <div className="col-span-6 md:col-span-3 text-2xl md:text-xl">
+                    .01
+                </div>
+                <div className="col-span-6 md:col-span-9">
+                    <div className="grid md:grid-cols-9 grid-cols-6 gap-y-5 md:gap-x-10 md:gap-y-5">
+                        <div className="hidden col-span-9 md:block">
+                            <h2 className={`${fontDisplay.className} text-7xl`}>
+                                Where Creativity Meets Technical Expertise
+                            </h2>
+                        </div>
+
+                        <div className="block leading-none col-span-9 md:hidden text-[10.572vw]">
+                            <h2 className={`${fontDisplay.className}`}>
+                                Where Creativity
+                            </h2>
+                            <h2 className={`${fontDisplay.className}`}>
+                                Meets Technical
+                            </h2>
+                            <h2 className={`${fontDisplay.className}`}>
+                                Expertise
+                            </h2>
+                        </div>
+                        <span
+                            className={`${fontDisplayItalic.className} col-span-6 md:col-span-3 text-xl md:text-2xl underline`}
                         >
-                            ariefsatrio098@gmail.com.
-                        </Link>
-                    </p>
+                            intro
+                        </span>
+                        <div className="col-span-6 md:col-span-4 flex-col flex gap-2 md:text-base">
+                            <p>About.</p>
+                            <p>
+                                Arief is a full-stack developer and graphic
+                                designer who combines artistic vision with
+                                technical skill. His love for design shows in
+                                his bold typography and interest in brutalism,
+                                always pushing the limits of visual style.
+                            </p>
+                            <p>
+                                Arief is committed to using his skills to create
+                                meaningful solutions in the industry. He enjoys
+                                working with talented people and using his
+                                expertise to drive innovation and quality in
+                                every project. Feel free to connect with him at{" "}
+                                <Link
+                                    className="underline underline-offset-1"
+                                    href={"mailto:ariefsatrio098@gmail.com"}
+                                >
+                                    ariefsatrio098@gmail.com.
+                                </Link>
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
