a6238fa6f595dbde2fee0ae3eacca2bd9b3aaf4c
arief satrio <ariefsatrio098@gmail.com>
2024-09-30 11:58:38 +0700

    fix: bug text content doesnt match the render on server

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,20 @@ export const metadata: Metadata = {
     },
 };
 
+const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "Person",
+    name: "Arief Satrio",
+    jobTitle: "Full Stack Developer, Graphic Designer",
+    url: "https://ariefsatrio.com",
+    sameAs: [
+        "https://www.linkedin.com/in/arief-satrio-budi-prasojo",
+        "https://github.com/kurodoke",
+    ],
+    description:
+        "Arief Satrio is a Full Stack Developer and Graphic Designer with expertise in combines artistic vision with technical skill.",
+};
+
 export default function RootLayout({
     children,
 }: Readonly<{
@@ -30,21 +44,10 @@ export default function RootLayout({
     return (
         <html lang="en">
             <head>
-                <script type="application/ld+json">
-                    {JSON.stringify({
-                        "@context": "https://schema.org",
-                        "@type": "Person",
-                        name: "Arief Satrio",
-                        jobTitle: "Full Stack Developer, Graphic Designer",
-                        url: "https://ariefsatrio.com",
-                        sameAs: [
-                            "https://www.linkedin.com/in/arief-satrio-budi-prasojo",
-                            "https://github.com/kurodoke",
-                        ],
-                        description:
-                            "Arief Satrio is a Full Stack Developer and Graphic Designer with expertise in combines artistic vision with technical skill.",
-                    })}
-                </script>
+                <script
+                    type="application/ld+json"
+                    dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+                />
             </head>
             <body
                 className={`${fontText.className} bg-custom-gray relative min-h-screen overflow-y-scroll overflow-x-hidden`}
